d5b5eab918c1d4a4e5e9a804a7ccf1913bb0b967
doc999tor <doc999tor@gmail.com>
2020-09-16 03:02:39 +0300

    gitignore

--- bin/utils/query-helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serializeQueryString = void 0;
-function serializeQueryString(body) {
-    return Object.entries(body)
-        .map(kv => encodeURIComponent(kv[0]) + '=' + encodeURIComponent(kv[1].toString()))
-        .join('&');
-}
-exports.serializeQueryString = serializeQueryString;
